b35a666421fb870dca978807e01a845a9fe3637c
Chribtoper <xcstolfax@gmail.com>
2018-06-27 23:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function makeInt(n){
 function preserveDecimal(n){
   return parseFloat(n)
 }
+
+function addition(a, b){
+  return add(a, b)
+}
+
+function subtractNum(a, b){
+  return subtract(a, b)
+}
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function division(a, b){
+  return divide(a, b)
+}
+
+function multiplication(a, b){
+  return multiply(a, b)
+}
